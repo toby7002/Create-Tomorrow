1e4c75efe6e133108fc381dea53da40c0cba3cdc
toby7002 <144540995+toby7002@users.noreply.github.com>
2024-03-11 20:42:46 +0700

    Hide and remove items

---
+++ kubejs/client_scripts/index.js
@@ -0,0 +1,112 @@
+"use strict";
+JEIEvents.hideItems(function (event) {
+	getMekanism()
+		.concat(getCreateNewAge(), getCreateAddition(), getTwilightForest())
+		.forEach(function (item) {
+			event.hide(item);
+		});
+});
+function getMekanism() {
+	var materials = [
+		"refined_obsidian",
+		"refined_glowstone",
+		"bronze",
+		"lapis_lazuli",
+		"osmium",
+		"steel",
+	];
+	function getMekanismItems() {
+		return [
+			"qio_drive_array",
+			"qio_dashboard",
+			"qio_exporter",
+			"qio_importer",
+			"qio_redstone_adapter",
+			"portable_qio_dashboard",
+			"qio_drive_base",
+			"qio_drive_hyper_dense",
+			"qio_drive_time_dilating",
+			"qio_drive_supermassive",
+			"scuba_mask",
+			"scuba_tank",
+		].map(function (item) {
+			return "mekanism:".concat(item);
+		});
+	}
+	function getMekanismTools() {
+		var types = ["swords", "pickaxes", "hoes", "axes", "shovels"];
+		return materials
+			.map(function (material) {
+				return types.map(function (type) {
+					return "tools/".concat(type, "/").concat(material);
+				});
+			})
+			.reduce(function (prev, curr) {
+				return prev.concat(curr);
+			}, [])
+			.map(function (item) {
+				return "#forge:".concat(item);
+			});
+	}
+	function getMekanismArmors() {
+		var types = ["helmets", "chestplates", "leggings", "boots"];
+		return materials
+			.map(function (material) {
+				return types.map(function (type) {
+					return "armors/".concat(type, "/").concat(material);
+				});
+			})
+			.reduce(function (prev, curr) {
+				return prev.concat(curr);
+			}, [])
+			.map(function (item) {
+				return "#forge:".concat(item);
+			});
+	}
+	return getMekanismItems().concat(getMekanismTools(), getMekanismArmors());
+}
+function getCreateNewAge() {
+	function getCreateNewAgeItems() {
+		return [
+			"energiser_t1",
+			"energiser_t2",
+			"energiser_t3",
+			"carbon_brushes",
+			"generator_coil",
+			"magnetite_block",
+			"redstone_magnet",
+			"layered_magnet",
+			"fluxuated_magnetite",
+			"netherite_magnet",
+		].map(function (item) {
+			return "create_new_age:".concat(item);
+		});
+	}
+	return getCreateNewAgeItems();
+}
+function getCreateAddition() {
+	function getCreateAdditionItems() {
+		return [
+			"electric_motor",
+			"connector",
+			"large_connector",
+			"small_light_connector",
+			"spool",
+			"copper_spool",
+			"gold_spool",
+			"electrum_spool",
+			"festive_spool",
+		].map(function (item) {
+			return "createaddition:".concat(item);
+		});
+	}
+	return getCreateAdditionItems();
+}
+function getTwilightForest() {
+	function getTwilightForestItems() {
+		return ["uncrafting_table"].map(function (item) {
+			return "twilightforest:".concat(item);
+		});
+	}
+	return getTwilightForestItems();
+}
---
+++ kubets/server_scripts/recipes.ts
@@ -0,0 +1,108 @@
+ServerEvents.recipes((event) => {
+	getMekanism()
+		.concat(getCreateNewAge(), getCreateAddition(), getTwilightForest())
+		.forEach((item: string) => {
+			event.remove({ output: item as any });
+		});
+});
+
+function getMekanism(): string[] {
+	const materials: string[] = [
+		"refined_obsidian",
+		"refined_glowstone",
+		"bronze",
+		"lapis_lazuli",
+		"osmium",
+		"steel",
+	];
+
+	function getMekanismItems(): string[] {
+		return [
+			"qio_drive_array",
+			"qio_dashboard",
+			"qio_exporter",
+			"qio_importer",
+			"qio_redstone_adapter",
+			"portable_qio_dashboard",
+			"qio_drive_base",
+			"qio_drive_hyper_dense",
+			"qio_drive_time_dilating",
+			"qio_drive_supermassive",
+			"scuba_mask",
+			"scuba_tank",
+		].map((item) => `mekanism:${item}`);
+	}
+
+	function getMekanismTools(): string[] {
+		const types: string[] = [
+			"swords",
+			"pickaxes",
+			"hoes",
+			"axes",
+			"shovels",
+		];
+
+		return materials
+			.map((material) => types.map((type) => `tools/${type}/${material}`))
+			.reduce((prev, curr) => prev.concat(curr), [])
+			.map((item) => `#forge:${item}`);
+	}
+
+	function getMekanismArmors(): string[] {
+		const types: string[] = ["helmets", "chestplates", "leggings", "boots"];
+
+		return materials
+			.map((material) =>
+				types.map((type) => `armors/${type}/${material}`),
+			)
+			.reduce((prev, curr) => prev.concat(curr), [])
+			.map((item) => `#forge:${item}`);
+	}
+
+	return getMekanismItems().concat(getMekanismTools(), getMekanismArmors());
+}
+
+function getCreateNewAge(): string[] {
+	function getCreateNewAgeItems(): string[] {
+		return [
+			"energiser_t1",
+			"energiser_t2",
+			"energiser_t3",
+			"carbon_brushes",
+			"generator_coil",
+			"magnetite_block",
+			"redstone_magnet",
+			"layered_magnet",
+			"fluxuated_magnetite",
+			"netherite_magnet",
+		].map((item) => `create_new_age:${item}`);
+	}
+
+	return getCreateNewAgeItems();
+}
+
+function getCreateAddition(): string[] {
+	function getCreateAdditionItems(): string[] {
+		return [
+			"electric_motor",
+			"connector",
+			"large_connector",
+			"small_light_connector",
+			"spool",
+			"copper_spool",
+			"gold_spool",
+			"electrum_spool",
+			"festive_spool",
+		].map((item) => `createaddition:${item}`);
+	}
+
+	return getCreateAdditionItems();
+}
+
+function getTwilightForest(): string[] {
+	function getTwilightForestItems(): string[] {
+		return ["uncrafting_table"].map((item) => `twilightforest:${item}`);
+	}
+
+	return getTwilightForestItems();
+}
